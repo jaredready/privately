5413ad3cd8520cf860a985bb9b8cc5b3e41ac5fd
jaredready <jready@adaptiverx.com>
2020-05-02 19:45:44 -0500

    Initial work on GraphQL API

--- lib/backend-stack.ts
+++ lib/backend-stack.ts
@@ -1,18 +1,106 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as appsync from "@aws-cdk/aws-appsync";
+import { Table, AttributeType } from "@aws-cdk/aws-dynamodb";
+import { resolve } from "path";
+import { Role, ServicePrincipal, ManagedPolicy } from "@aws-cdk/aws-iam";
+import {
+  DynamoDbDataSource,
+  Resolver,
+  MappingTemplate,
+  GraphQLApi,
+  BaseDataSource,
+} from "@aws-cdk/aws-appsync";
+import * as fs from "fs";
+import { Construct } from "@aws-cdk/core";
+import { create } from "domain";
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'BackendQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const graphqlApi = new appsync.GraphQLApi(this, "GraphQlApi", {
+      name: "prod-privately",
+      schemaDefinitionFile: resolve(__dirname, "schema.graphql"),
     });
 
-    const topic = new sns.Topic(this, 'BackendTopic');
+    const ddbTable = new Table(this, "Table", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // TODO disable for production
+      tableName: "prod-app-table",
+      partitionKey: {
+        name: "partitionKey",
+        type: AttributeType.STRING,
+      },
+      sortKey: {
+        name: "sortKey",
+        type: AttributeType.STRING,
+      },
+      readCapacity: 5,
+      writeCapacity: 5,
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const tableAccessRole = new Role(this, "TableAccessRole", {
+      assumedBy: new ServicePrincipal("appsync.amazonaws.com"),
+    });
+
+    // TODO create custom restricted policy
+    tableAccessRole.addManagedPolicy(
+      ManagedPolicy.fromAwsManagedPolicyName("AmazonDynamoDBFullAccess")
+    );
+
+    const ddbTableDataSource = new DynamoDbDataSource(
+      this,
+      "DdbTableDataSource",
+      {
+        table: ddbTable,
+        api: graphqlApi,
+        name: "dynamodb",
+        serviceRole: tableAccessRole,
+      }
+    );
+
+    const query_maskedEmailAddresses = createResolver(
+      this,
+      graphqlApi,
+      ddbTableDataSource,
+      "Query",
+      "maskedEmailAddresses"
+    );
+
+    const mutation_createMaskedEmailAddress = createResolver(
+      this,
+      graphqlApi,
+      ddbTableDataSource,
+      "Mutation",
+      "createMaskedEmailAddress"
+    );
   }
 }
+
+const createResolver = (
+  construct: Construct,
+  api: GraphQLApi,
+  dataSource: BaseDataSource,
+  typeName: string,
+  fieldName: string
+) => {
+  return new Resolver(construct, `${typeName}.${fieldName} Resolver`, {
+    api,
+    dataSource,
+    fieldName,
+    typeName,
+    requestMappingTemplate: MappingTemplate.fromFile(
+      resolve(
+        __dirname,
+        "mappingTemplates/",
+        `${typeName}.${fieldName}.request.vtl`
+      )
+    ),
+    responseMappingTemplate: MappingTemplate.fromFile(
+      resolve(
+        __dirname,
+        "mappingTemplates/",
+        `${typeName}.${fieldName}.response.vtl`
+      )
+    ),
+  });
+};
--- test/backend.test.ts
+++ test/backend.test.ts
@@ -1,21 +1,57 @@
-import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as Backend from '../lib/backend-stack';
+import { expect as expectCDK, haveResource } from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as Backend from "../lib/backend-stack";
 
-test('SQS Queue Created', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new Backend.BackendStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(haveResource("AWS::SQS::Queue",{
-      VisibilityTimeout: 300
-    }));
-});
-
-test('SNS Topic Created', () => {
+test("AppSync GraphQl API Created", () => {
   const app = new cdk.App();
   // WHEN
-  const stack = new Backend.BackendStack(app, 'MyTestStack');
+  const stack = new Backend.BackendStack(app, "MyTestStack");
   // THEN
-  expectCDK(stack).to(haveResource("AWS::SNS::Topic"));
+  expectCDK(stack).to(haveResource("AWS::AppSync::GraphQLApi"));
+});
+
+test("DynamoDb Table Created", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new Backend.BackendStack(app, "MyTestStack");
+  // THEN
+  expectCDK(stack).to(haveResource("AWS::DynamoDB::Table"));
+});
+
+test("AppSync DynamoDb Data Source Created", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new Backend.BackendStack(app, "MyTestStack");
+  // THEN
+  expectCDK(stack).to(
+    haveResource("AWS::AppSync::DataSource", {
+      Type: "AMAZON_DYNAMODB",
+    })
+  );
+});
+
+test("Query.maskedEmailAddresses Resolver Created", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new Backend.BackendStack(app, "MyTestStack");
+  // THEN
+  expectCDK(stack).to(
+    haveResource("AWS::AppSync::Resolver", {
+      TypeName: "Query",
+      FieldName: "maskedEmailAddresses",
+    })
+  );
+});
+
+test("Mutation.createMaskedEmailAddress Resolver Created", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new Backend.BackendStack(app, "MyTestStack");
+  // THEN
+  expectCDK(stack).to(
+    haveResource("AWS::AppSync::Resolver", {
+      TypeName: "Mutation",
+      FieldName: "createMaskedEmailAddress",
+    })
+  );
 });
